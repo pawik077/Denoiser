e6b65cf87e7b698cba23adb85a018fb718f7a677
pawik077 <pawik.r.s.077@gmail.com>
2023-03-24 18:44:05 +0100

    Implemented training

--- train.py
+++ train.py
@@ -5,12 +5,9 @@ from helpers import *
 import augmentation
 from REDNet import REDNet_model
 
-def train(model: tf.keras.Model, model_name: str):
+def train(model: tf.keras.Model, datasets, augmentations,  filename: str, epochs: int = 200):
     # load data
-    sidd_dir = './datasets/SIDD'
-    renoir_dir = './datasets/RENOIR'
-    nind_dir = './datasets/NIND'
-    gt_paths, noisy_paths = get_img_paths(sidd_dir, renoir_dir, nind_dir)
+    gt_paths, noisy_paths = get_img_paths(datasets)
 
     #split data
     gt_paths_train, gt_paths_test, noisy_paths_train, noisy_paths_test = sk.train_test_split(gt_paths, noisy_paths, test_size=0.2)
@@ -22,24 +19,22 @@ def train(model: tf.keras.Model, model_name: str):
     noisy_test_imgs = load_images(noisy_paths_test)
 
     #augment data
-    augmentations = [augmentation.up_down_flip, augmentation.left_right_flip, augmentation.rotate]
     train_set = augmentation.dataset_generator(x=noisy_train_imgs, y=gt_train_imgs, batch_size=4, augmentations=augmentations)
     test_set = augmentation.dataset_generator(x=noisy_test_imgs, y=gt_test_imgs, batch_size=4, augmentations=None)
 
     #train model
-    model_path = f'./models/{model_name}.h5'
+    model_path = f'./models/{filename}.h5'
     callbacks = [
         tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, min_delta=0.0001, patience=10),
         tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', mode='min', factor=0.8, patience=3, verbose=1, min_lr=1e-6, min_delta=0.0001),
         tf.keras.callbacks.ModelCheckpoint(filepath=model_path, monitor='val_loss', mode='min', save_best_only=True, save_weights_only=False, frequency='epoch'),      
     ]
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss=tf.keras.losses.MeanSquaredError(), metrics=[tf.keras.metrics.MeanSquaredError()])
-    model.fit(train_set, validation_data=test_set, epochs=200, steps_per_epoch=len(noisy_train_imgs), validation_steps=len(noisy_test_imgs), verbose=1, callbacks=callbacks)
+    model.fit(train_set, validation_data=test_set, epochs=epochs, steps_per_epoch=len(noisy_train_imgs), validation_steps=len(noisy_test_imgs), verbose=1, callbacks=callbacks)
 
     #save model
     model.save(model_path)
 
 if __name__ == '__main__':
-    model_name = 'REDNet'
-    model = REDNet_model()
-    train(model, model_name)
+    # for testing purposes
+    train(REDNet_model(), ['SIDD', 'RENOIR', 'NIND'], [augmentation.up_down_flip, augmentation.left_right_flip, augmentation.rotate], 'REDNet')
