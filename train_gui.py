c2f49bc17c1a372de3520990f0648484a60f1122
pawik077 <pawik.r.s.077@gmail.com>
2023-03-24 20:07:26 +0100

    Beautified gui

--- train_gui.py
+++ train_gui.py
@@ -11,7 +11,7 @@ class GUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("GUI")
-        self.geometry("500x500")
+        self.geometry("330x230")
         self.datasets = {
             "SIDD": tk.BooleanVar(value=True),
             "RENOIR": tk.BooleanVar(value=True),
@@ -29,18 +29,8 @@ class GUI(tk.Tk):
         self.create_widgets()
 
     def create_widgets(self):
-        self.btn_debug = ttk.Button(self, text="Debug", command=self.debug)
-        self.btn_debug.pack()
-        
-        self.frm_datasets = tk.Frame(self)
-        self.frm_datasets.pack()
-        self.lbl_datasets = ttk.Label(self.frm_datasets, text="Datasets:")
-        self.lbl_datasets.pack()
-        self.cbs_datasets = [tk.Checkbutton(self.frm_datasets, text=k, variable=v, onvalue=True, offvalue=False) for (k,v) in self.datasets.items()]
-        for _, cb in enumerate(self.cbs_datasets): cb.pack()
-
         self.frm_model = tk.Frame(self)
-        self.frm_model.pack()
+        self.frm_model.place(x=0, y=0)
 
         self.lbl_model = ttk.Label(self.frm_model, text="Model:")
         self.lbl_model.pack()
@@ -49,16 +39,8 @@ class GUI(tk.Tk):
         self.cbb_model.pack()
         self.cbb_model.current(0)
 
-        self.frm_augment = tk.Frame(self)
-        self.frm_augment.pack()
-
-        self.lbl_augment = ttk.Label(self.frm_augment, text="Augmentations:")
-        self.lbl_augment.pack()
-        self.cbs_augment = [tk.Checkbutton(self.frm_augment, text=k, variable=v, onvalue=True, offvalue=False) for (k,v) in self.augmentations.items()]
-        for _, cb in enumerate(self.cbs_augment): cb.pack()
-
         self.frm_filename = tk.Frame(self)
-        self.frm_filename.pack()
+        self.frm_filename.place(x=7, y=40)
 
         self.lbl_filename = ttk.Label(self.frm_filename, text="Filename:")
         self.lbl_filename.pack()
@@ -67,7 +49,7 @@ class GUI(tk.Tk):
         self.ent_filename.pack()
 
         self.frm_epochs = tk.Frame(self)
-        self.frm_epochs.pack()
+        self.frm_epochs.place(x=7, y=80)
 
         self.lbl_epochs = ttk.Label(self.frm_epochs, text="Epochs:")
         self.lbl_epochs.pack()
@@ -75,11 +57,30 @@ class GUI(tk.Tk):
         self.ent_epochs.insert(0, 200)
         self.ent_epochs.pack()
 
-        self.btn_train = ttk.Button(self, text="Train", command=self.train_trigger)
-        self.btn_train.pack()
+        self.frm_datasets = tk.Frame(self)
+        self.frm_datasets.place(x=0, y=120)
+
+        self.lbl_datasets = ttk.Label(self.frm_datasets, text="Datasets:")
+        self.lbl_datasets.pack()
+        self.cbs_datasets = [tk.Checkbutton(self.frm_datasets, text=k, variable=v, onvalue=True, offvalue=False) for (k,v) in self.datasets.items()]
+        for _, cb in enumerate(self.cbs_datasets): cb.pack(anchor=tk.W)
+
+        self.btn_train = ttk.Button(self, text="Train", command=self.train_trigger, )
+        self.btn_train.place(x=85, y=140)
+
+        self.btn_debug = ttk.Button(self, text="Debug", command=self.debug)
+        self.btn_debug.place(x=85, y=170)
+        
+        self.frm_augment = tk.Frame(self)
+        self.frm_augment.place(x=180, y=0)
+
+        self.lbl_augment = ttk.Label(self.frm_augment, text="Augmentations:")
+        self.lbl_augment.pack()
+        self.cbs_augment = [tk.Checkbutton(self.frm_augment, text=k, variable=v, onvalue=True, offvalue=False) for (k,v) in self.augmentations.items()]
+        for _, cb in enumerate(self.cbs_augment): cb.pack(anchor=tk.W)
         
         self.lbl_processing = ttk.Label(self, text='')
-        self.lbl_processing.pack()
+        self.lbl_processing.place(x=7, y=210)
 
     def train_trigger(self):
         self.lbl_processing.config(text="Training model (consult terminal)...")
