586689bf2347f781154bd9a6fae0dcc88482b020
pawik077 <pawik.r.s.077@gmail.com>
2023-05-06 14:30:29 +0200

    Implemented options for inference

--- infer.py
+++ infer.py
@@ -5,14 +5,21 @@ import matplotlib.pyplot as plt
 import random
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
+import sys
+import getopt
 
 from helpers import *
+from MWCNN import DWT_downsampling, IWT_upsampling, Conv_block
+from PRIDNet import Convolution_block, Channel_attention, Avg_pool_Unet_Upsample_msfe, Multi_scale_feature_extraction, Kernel_selecting_module
 
-def infer_one(model: tf.keras.Model, noisy_img: np.ndarray):
-    input = np.expand_dims(noisy_img, axis=0)
-    denoised_img = model.predict(input)
-    denoised_img = (255.0*(denoised_img[0] - np.min(denoised_img[0]))/np.ptp(denoised_img[0])).astype(np.uint8)
-    return denoised_img[0]
+def load_images(paths):
+    imgs = []
+    for path in paths:
+        img = cv.imread(path)
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        img = cv.resize(img, (256, 256))
+        imgs.append(img)
+    return np.asarray(imgs, dtype=np.uint8)
 
 def infer(model: tf.keras.Model, noisy_imgs: np.ndarray):
     denoised_imgs = model.predict(noisy_imgs, batch_size=4)
@@ -36,36 +43,81 @@ def visualize(noisy_img: np.ndarray, denoised_img: np.ndarray, clean_img: np.nda
         plt.show()
 
 if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'm:rs:n:f:', ['model=', 'random', 'seed=', 'number=', 'file='])
+    except getopt.GetoptError as err:
+        print(err)
+        exit(2)
+    
+    model_name = None
+    rand = False
+    seed = None
+    number = None
+    file = None
+
+    for o, a in opts:
+        if o in ('-m', '--model'):
+            model_name = a
+        elif o in ('-r', '--random'):
+            rand = True
+        elif o in ('-s', '--seed'):
+            seed = int(a)
+        elif o in ('-n', '--number'):
+            number = int(a)
+        elif o in ('-f', '--file'):
+            file = a
+
+    if model_name == None:
+        raise ValueError('Model name not specified')
+    if rand == False and file == None:
+        raise ValueError('No input specified')
+    if rand == True and file != None:
+        raise ValueError('Cannot specify both random and file')
+    if rand == False and seed != None:
+        raise ValueError('Cannot specify seed without random')
+    if rand == False and number != None:
+        raise ValueError('Cannot specify number without random')
+    if rand == True and number == None:
+        raise ValueError('Number not specified')
+    
     # load model
-    model_name = 'REDNet'
-    model = tf.keras.models.load_model(f'./models/{model_name}.h5')
+    if 'REDNet' in model_name:
+        model = tf.keras.models.load_model(f'./models/{model_name}.h5')
+    elif 'MWCNN' in model_name:
+        model = tf.keras.models.load_model(f'./models/{model_name}.h5', custom_objects={'DWT_downsampling': DWT_downsampling, 'IWT_upsampling': IWT_upsampling, 'Conv_block': Conv_block})
+    elif 'PRIDNet' in model_name:
+        model = tf.keras.models.load_model(f'./models/{model_name}.h5', custom_objects={'Convolution_block': Convolution_block, 'Channel_attention': Channel_attention, 'Avg_pool_Unet_Upsample_msfe': Avg_pool_Unet_Upsample_msfe, 'Multi_scale_feature_extraction': Multi_scale_feature_extraction, 'Kernel_selecting_module': Kernel_selecting_module})
+    else:
+        raise ValueError('Model not recognized')
+    
+    if rand == True:
+        gt_paths, noisy_paths = get_img_paths(['SIDD', 'RENOIR', 'NIND'])
+        choices = random.choices(range(len(gt_paths)), k=number)
+        gt_images = load_images([gt_paths[i] for i in choices])
+        noisy_images = load_images([noisy_paths[i] for i in choices])
+    else:
+        number = 1
+        noisy_images = load_images([file])
 
-    # load data
-    gt_paths, noisy_paths = get_img_paths(['SIDD', 'RENOIR', 'NIND'])
+    denoised_images = infer(model, noisy_images)
 
-    # load data
-    choices = random.choices(range(len(gt_paths)), k=10)
-    gt_imgs = load_images([gt_paths[i] for i in choices])
-    noisy_imgs = load_images([noisy_paths[i] for i in choices])
-
-    # infer
-    denoised_imgs = infer(model, noisy_imgs)
-    print(f'Paths: \n{[noisy_paths[i] for i in choices]}')
-
-    # metrics
-    psnr_gt_mean = np.mean([psnr(gt_imgs[i], noisy_imgs[i]) for i in range(len(gt_imgs))])
-    psnr_denoised_mean = np.mean([psnr(gt_imgs[i], denoised_imgs[i]) for i in range(len(gt_imgs))])
-    ssim_gt_mean = np.mean([ssim(gt_imgs[i], noisy_imgs[i], channel_axis=-1, data_range=noisy_imgs[i].max() - noisy_imgs[i].min()) for i in range(len(gt_imgs))])
-    ssim_denoised_mean = np.mean([ssim(gt_imgs[i], denoised_imgs[i], channel_axis=-1, data_range=denoised_imgs[i].max() - denoised_imgs[i].min()) for i in range(len(gt_imgs))])
-    print(f'PSNR (GT): {psnr_gt_mean}')
-    print(f'PSNR (Denoised): {psnr_denoised_mean}')
-    print(f'SSIM (GT): {ssim_gt_mean}')
-    print(f'SSIM (Denoised): {ssim_denoised_mean}')
-
-    # visualize
-    for i in range(10):
-        visualize(noisy_imgs[i], denoised_imgs[i], gt_imgs[i], f'{model_name}_{i}_plot')
-        denoised_imgs[i] = cv.cvtColor(denoised_imgs[i], cv.COLOR_RGB2BGR) # openCV uses BGR, so we need to convert back
-        is_saved = cv.imwrite(f'./results/{model_name}_{i}.png', denoised_imgs[i])
+    if rand:
+        psnr_gt_mean = np.mean([psnr(gt_images[i], noisy_images[i]) for i in range(len(gt_images))])
+        psnr_denoised_mean = np.mean([psnr(gt_images[i], denoised_images[i]) for i in range(len(gt_images))])
+        ssim_gt_mean = np.mean([ssim(gt_images[i], noisy_images[i], channel_axis=-1, data_range=noisy_images[i].max() - noisy_images[i].min()) for i in range(len(gt_images))])
+        ssim_denoised_mean = np.mean([ssim(gt_images[i], denoised_images[i], channel_axis=-1, data_range=denoised_images[i].max() - denoised_images[i].min()) for i in range(len(gt_images))])
+        print(f'PSNR (GT): {psnr_gt_mean}')
+        print(f'PSNR (Denoised): {psnr_denoised_mean}')
+        print(f'SSIM (GT): {ssim_gt_mean}')
+        print(f'SSIM (Denoised): {ssim_denoised_mean}')
+    # else:
+    #     psnr_denoised_mean = psnr(gt_images[0], denoised_images[0])
+    #     ssim_denoised_mean = ssim(gt_images[0], denoised_images[0], channel_axis=-1, data_range=denoised_images[0].max() - denoised_images[0].min())
+    #     print(f'PSNR (Denoised): {psnr_denoised_mean}')
+    #     print(f'SSIM (Denoised): {ssim_denoised_mean}')
+    for i in range(number):
+        visualize(noisy_images[i], denoised_images[i], gt_images[i], f'{model_name}_{i}_plot')
+        denoised_images[i] = cv.cvtColor(denoised_images[i], cv.COLOR_RGB2BGR)
+        is_saved = cv.imwrite(f'./results/{model_name}_{i}.png', denoised_images[i])
         if not is_saved:
             print(f'Failed to save {model_name}_{i}.png')
