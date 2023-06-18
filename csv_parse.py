938926fa2411e2a72e155b9d0c3da57f58af5b17
pawik077 <pawik.r.s.077@gmail.com>
2023-06-18 20:03:43 +0200

    Added chart generation from csv

--- csv_parse.py
+++ csv_parse.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import matplotlib.pyplot as plt
 
 data = []
 with open(f'test/{sys.argv[1]}/results.csv', 'r') as f:
@@ -8,8 +9,31 @@ with open(f'test/{sys.argv[1]}/results.csv', 'r') as f:
         data.append(row)
 
 # print(data)
-print('Model & PSNR & SSIM & Poprawa PSNR & Poprawa SSIM \\\\ \hline')
-for row in data:
-    if row[0] == 'Model':
-        continue
-    print(f'{row[0]} & {float(row[1]):.4f} & {float(row[2]):.4f} & {float(data[1][1]) - float(row[1]):.4f} & {float(data[1][2]) - float(row[2]):.4f} \\\\ \hline')
+if sys.argv[2] == 'table':
+    print('Model & PSNR & SSIM & Poprawa PSNR & Poprawa SSIM \\\\ \hline')
+    for row in data:
+        if row[0] == 'Model':
+            continue
+        print(f'{row[0]} & {float(row[1]):.4f} & {float(row[2]):.4f} & {float(data[1][1]) - float(row[1]):.4f} & {float(data[1][2]) - float(row[2]):.4f} \\\\ \hline')
+elif sys.argv[2] == 'chart':
+    models = []
+    psnrs = []
+    ssims = []
+    mses = []
+    for row in data:
+        if row[0] == 'Model':
+            continue
+        models.append(row[0])
+        psnrs.append(float(row[1]))
+        ssims.append(float(row[2]))
+        mses.append(float(row[3]))
+    plt.figure(0)
+    plt.title('PSNR')
+    plt.bar(models, psnrs)
+    plt.figure(1)
+    plt.title('SSIM')
+    plt.bar(models, ssims)
+    plt.figure(2)
+    plt.title('MSE')
+    plt.bar(models, mses)
+    plt.show()
